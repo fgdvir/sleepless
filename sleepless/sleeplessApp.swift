498300106ffa7df29e3716cfc516ce4bcbcfc9ff
dvir <dvir.itzko@gmail.com>
2024-10-20 21:04:28 +0300

    Initial Commit

---
+++ sleepless/sleeplessApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleeplessApp.swift
+//  sleepless
+//
+//  Created by Dvir Itzkovits on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct sleeplessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
